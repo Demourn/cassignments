d4169ad5a3cfb204f94581be98e0c9d1ec323af2
Demourn <parthibdutta59@gmail.com>
2025-05-15 22:08:29 +0530

    program to find the sum of a harmonic series

---
+++ series.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  double sum = 1;
+  int n = 0;
+  printf("Enter a number: ");
+  scanf("%d", &n);
+  printf("The sum of series is: ");
+  for (int i = 1; i <=n; i++){
+    sum += 1.0/i;
+  }
+  printf("%f", sum);
+}
